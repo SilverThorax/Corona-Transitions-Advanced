070e222c1dc64c87672f995895eab9037ad4f3f5
davidroulin <freelance@davidroulin.ch>
2016-07-23 14:20:51 +0200

    .restart() in progress

--- main.lua
+++ main.lua
@@ -1,27 +1,63 @@
-require('_dont_commit_utils')
 
 require( "transition3" )
 
-local r = display.newRect( 100, 100, 60, 60 )
+local rwh = 60
 
-local t = transition.to( r, { x = r.x + 200, y = r.y + 600, time = 2000, tag = 'truc', onComplete = function()
-	print("finish")
-end } )
+local r = display.newRect( rwh/2, rwh/2, rwh, rwh )
+
+local t = transition.to( r, { x = display.viewableContentWidth - rwh/2, y = display.viewableContentHeight - rwh/2, time = 2000, tag = 'truc'
+	,onComplete = function()
+		print("Complete")
+	end
+	,onStart = function()
+		print("Start")
+	end
+	,onResume = function()
+		print("Resume")
+	end
+	,onPause = function()
+		print("Pause")
+	end
+	,onCancel = function()
+		print("Cancel")
+	end
+} )
+
 
 timer.performWithDelay( 1000, function()
 	-- transition.pause( t )
 	-- transition.pause()
 	-- transition.pause( r )
-	transition.pause( 'truc' )
+	-- transition.pause( 'truc' )
+
+	-- print( "" )
+	-- print( "" )
+	-- print( "PAUSE" )
+	-- print( "" )
+	-- print( "" )
+
+	-- t._transition = function()
+	-- 	print("pssht")
+	-- end
+
+	-- transition.resume( t )
+
+	transition.restart( t )
+
 end )
 
-timer.performWithDelay( 2000, function()
+timer.performWithDelay( 1600, function()
 	-- transition.resume( t )
 	-- transition.resume()
 
 	-- transition.complete( t )
 	-- transition.complete()
 	-- transition.complete( r )
-	transition.complete( 'truc' )
+	-- transition.complete( 'truc' )
+
+	-- transition.restart( t )
+
+	transition.pause( t )
 
 end )
+
--- transition3.lua
+++ transition3.lua
@@ -33,31 +33,86 @@ function transition.complete( ... )
 	-- processing
 	if #ts > 0 then
 		for i,t in ipairs(ts) do
+			-- prevent pause callback from being called
 			if t._onPause then t._onPause = nil end
 			transition.pause( t )
+			-- set final values
 			for k,v in pairs( t._keysFinish ) do
 				t._target[k] = v
 			end
+			-- extract complete callback before cancel
 			local callback = t._onComplete
+			-- cancel
 			if t._onCancel then t._onCancel = nil end
 			transition.cancel( t )
+			-- call complete callback
 			if callback then pcall( callback ) end
 		end
 	end
 end
 
 function transition.restart( ... )
+	local ts = {}
+	-- gathering
 	if arg.n == 1 then
 		if type(arg[1]) == 'table' then
 			if arg[1]._transition then
 				-- transition reference
+				ts[1] = arg[1]
 			elseif arg[1]._class.addEventListener then
 				-- display object
+				for i,t in ipairs(transition._transitionTable) do
+					if t._target == arg[1] then
+						ts[#ts+1] = t
+					end
+				end
 			end
 		elseif type(arg[1]) == 'string' then
 			-- tag
+			for i,t in ipairs(transition._transitionTable) do
+				if t.tag == arg[1] then
+					ts[#ts+1] = t
+				end
+			end
 		end
 	elseif arg.n == 0 then
 		-- all
+		for i,t in ipairs(transition._transitionTable) do
+			ts[#ts+1] = t
+		end
+	end
+	-- processing
+	if #ts > 0 then
+		for i,t in ipairs(ts) do
+			-- save pause callback but prevent it from being called
+			local _pauseCallback = nil
+			if t._onPause then
+				_pauseCallback = t._onPause
+				t._onPause = nil
+			end
+			transition.pause( t )
+			if _pauseCallback then
+				t._onPause = _pauseCallback
+			end
+			-- set initial values
+			for k,v in pairs( t._keysStart ) do
+				t._target[k] = v
+			end
+			-- reset timer
+			t._timeStart = system.getTimer()
+			-- save pause callback but prevent it from being called
+			local _resumeCallback = nil
+			if t._onResume then
+				_resumeCallback = t._onResume
+				t._onResume = nil
+			end
+			transition.resume( t )
+			if _resumeCallback then
+				t._onResume = _resumeCallback
+			end
+			-- start callback
+			local callback = t._onStart
+			if callback then pcall( callback ) end
+		end
 	end
 end
