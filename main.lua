0fd653f2f7c32caca767d8588e08b70db4a57e92
davidroulin <freelance@davidroulin.ch>
2016-07-23 15:06:00 +0200

    cleaned up example code

--- main.lua
+++ main.lua
@@ -1,63 +1,41 @@
 
 require( "transition3" )
 
-local rwh = 60
+local size = 60
 
-local r = display.newRect( rwh/2, rwh/2, rwh, rwh )
+local myRectangle = display.newRect( size/2, size/2, size, size )
 
-local t = transition.to( r, { x = display.viewableContentWidth - rwh/2, y = display.viewableContentHeight - rwh/2, time = 2000, tag = 'truc'
-	,onComplete = function()
-		print("Complete")
-	end
-	,onStart = function()
-		print("Start")
-	end
-	,onResume = function()
-		print("Resume")
-	end
-	,onPause = function()
-		print("Pause")
-	end
-	,onCancel = function()
-		print("Cancel")
-	end
-} )
+local myTransition = transition.to(
+	myRectangle,
+	{
+		x = display.viewableContentWidth - size/2,
+		y = display.viewableContentHeight - size/2,
+		time = 3000,
+		tag = 'myTag',
+		onComplete = function() print("Complete") end,
+		onStart = function() print("Start") end,
+		onResume = function() print("Resume") end,
+		onPause = function() print("Pause") end,
+		onCancel = function() print("Cancel") end
+	}
+)
 
 
 timer.performWithDelay( 1000, function()
-	-- transition.pause( t )
-	-- transition.pause()
-	-- transition.pause( r )
-	-- transition.pause( 'truc' )
 
-	-- print( "" )
-	-- print( "" )
-	-- print( "PAUSE" )
-	-- print( "" )
-	-- print( "" )
-
-	-- t._transition = function()
-	-- 	print("pssht")
-	-- end
-
-	-- transition.resume( t )
-
-	transition.restart( t )
+	-- transition.restart( myTransition )
+	-- transition.restart()
+	-- transition.restart( myRectangle )
+	transition.restart( 'myTag' )
 
 end )
 
-timer.performWithDelay( 1600, function()
-	-- transition.resume( t )
-	-- transition.resume()
+timer.performWithDelay( 2000, function()
 
-	-- transition.complete( t )
+	-- transition.complete( myTransition )
 	-- transition.complete()
-	-- transition.complete( r )
-	-- transition.complete( 'truc' )
-
-	transition.restart( t )
-
-	-- transition.pause( t )
+	transition.complete( myRectangle )
+	-- transition.complete( 'myTag' )
 
 end )
 
